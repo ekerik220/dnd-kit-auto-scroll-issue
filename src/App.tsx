54cac4204dd8d098e78099fcb9d95c55cf07fe03
Evan Kerik <ekerik220@gmail.com>
2024-03-20 17:22:47 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,133 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { forwardRef, useState } from "react";
+
+import {
+  closestCenter,
+  DndContext,
+  DragEndEvent,
+  DraggableAttributes,
+  DraggableSyntheticListeners,
+  DragOverlay,
+  DragStartEvent,
+  KeyboardSensor,
+  MouseSensor,
+  TouchSensor,
+  UniqueIdentifier,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  useSortable,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <SortableGrid />;
 }
+
+const SortableGrid = () => {
+  const [activeId, setActiveId] = useState<UniqueIdentifier | null>(null);
+
+  const [items, setItems] = useState(
+    Array(300)
+      .fill(0)
+      .map((_, i) => i.toString())
+  );
+
+  const sensors = useSensors(
+    useSensor(MouseSensor),
+    useSensor(TouchSensor, {
+      activationConstraint: {
+        delay: 250,
+        tolerance: 5,
+      },
+    }),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+
+  function handleDragStart(event: DragStartEvent) {
+    const { active } = event;
+
+    setActiveId(active.id);
+  }
+
+  function handleDragEnd(event: DragEndEvent) {
+    const { active, over } = event;
+
+    if (over !== null && active.id !== over.id) {
+      setItems((items) => {
+        const oldIndex = items.indexOf(active.id.toString());
+        const newIndex = items.indexOf(over.id.toString());
+
+        return arrayMove(items, oldIndex, newIndex);
+      });
+    }
+  }
+
+  return (
+    <DndContext
+      sensors={sensors}
+      collisionDetection={closestCenter}
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
+    >
+      <SortableContext items={items} strategy={verticalListSortingStrategy}>
+        {items.map((item) => (
+          <SortableItem key={item} id={item} />
+        ))}
+      </SortableContext>
+      <DragOverlay>{activeId ? <Item value={activeId} /> : null}</DragOverlay>
+    </DndContext>
+  );
+};
+
+export const SortableItem = ({ id }: { id: UniqueIdentifier }) => {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
+  return (
+    <Item
+      ref={setNodeRef}
+      value={id}
+      style={style}
+      {...attributes}
+      {...listeners}
+    />
+  );
+};
+
+export interface Props {
+  style?: {
+    transform: string | undefined;
+    transition: string | undefined;
+  };
+  listeners?: DraggableSyntheticListeners;
+  attributes?: DraggableAttributes;
+  value: React.ReactNode;
+}
+
+const Item = forwardRef<HTMLLIElement, Props>(({ value, ...props }, ref) => {
+  return (
+    <li
+      ref={ref}
+      className="flex items-center justify-center list-none border size-20"
+      {...props}
+    >
+      {value}
+    </li>
+  );
+});
+
+Item.displayName = "Item";
 
 export default App;
